41fe224d62152417ee94d16ff0357dc93253b700
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-11-08 20:00:16 +0100

    Initial commit with minmal template from https://github.com/lewagon/rails-templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsHadley::Application.config.secret_token = '0dedba450f14027c72bd9f81a9f721eed03b7937b400208147004bff8b1407e6698fdc9b969523fcb3bc00b49cee7e84f8d93ec835932adcfadd2e8c94fd7350'
